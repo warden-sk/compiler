8721ebd296dc3d347ad474a0406731c881d00c0c
Marek Kobida <marek.kobida@gmail.com>
2023-03-29 14:33:03 +0200

    update

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ interface Options {
   assets?: string[];
   outputPath?: string;
   publicPath?: string;
-  transpileOnly?: boolean;
+  reportErrors?: boolean;
   useTransformers?: boolean;
 }
 
@@ -37,67 +37,67 @@ function compile(filePath: string, options: Options): string {
 
   const transformers: ts.CustomTransformers = { before: [cssTransformer(options), transformer()] };
 
-  if (options.transpileOnly) {
-    const { outputText: compiled } = ts.transpileModule(fs.readFileSync(filePath).toString(), {
-      compilerOptions,
-      fileName: filePath,
-      transformers: options.useTransformers ? (/compiler\//.test(filePath) ? undefined : transformers) : undefined,
-    });
+  if (options.reportErrors) {
+    let compiled = '';
+
+    const compilerHost: ts.CompilerHost = ts.createCompilerHost({});
+    compilerHost.writeFile = (fileName, text) => (compiled = text);
+
+    const program: ts.Program = ts.createProgram([filePath], compilerOptions, compilerHost);
+
+    const emitResult: ts.EmitResult = program.emit(
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      options.useTransformers ? (/compiler\//.test(filePath) ? undefined : transformers) : undefined
+    );
+
+    const diagnostics: ts.Diagnostic[] = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
 
     const endDate: number = +new Date();
 
-    report(
-      undefined,
-      '🟢',
-      `${((endDate - startDate) / 1000).toFixed(2)} second(s)`,
-      filePath,
-      sizeToReadable(compiled.length)
-    );
+    if (diagnostics.length > 0) {
+      for (const diagnostic of diagnostics) {
+        if (diagnostic.file) {
+          const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+
+          report(
+            undefined,
+            '🔴',
+            `${((endDate - startDate) / 1000).toFixed(2)} second(s)`,
+            `\x1b[31m${diagnostic.file.fileName}\n\n${message}\n\x1b[0m`
+          );
+        }
+      }
+    } else {
+      report(
+        undefined,
+        '🟢',
+        `${((endDate - startDate) / 1000).toFixed(2)} second(s)`,
+        filePath,
+        sizeToReadable(compiled.length)
+      );
+    }
 
     return compiled;
   }
 
-  let compiled = '';
-
-  const compilerHost: ts.CompilerHost = ts.createCompilerHost({});
-  compilerHost.writeFile = (fileName, text) => (compiled = text);
-
-  const program: ts.Program = ts.createProgram([filePath], compilerOptions, compilerHost);
-
-  const emitResult: ts.EmitResult = program.emit(
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    options.useTransformers ? (/compiler\//.test(filePath) ? undefined : transformers) : undefined
-  );
-
-  const diagnostics: ts.Diagnostic[] = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+  const { outputText: compiled } = ts.transpileModule(fs.readFileSync(filePath).toString(), {
+    compilerOptions,
+    fileName: filePath,
+    transformers: options.useTransformers ? (/compiler\//.test(filePath) ? undefined : transformers) : undefined,
+  });
 
   const endDate: number = +new Date();
 
-  if (diagnostics.length > 0) {
-    for (const diagnostic of diagnostics) {
-      if (diagnostic.file) {
-        const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-
-        report(
-          undefined,
-          '🔴',
-          `${((endDate - startDate) / 1000).toFixed(2)} second(s)`,
-          `\x1b[31m${diagnostic.file.fileName}\n\n${message}\n\x1b[0m`
-        );
-      }
-    }
-  } else {
-    report(
-      undefined,
-      '🟢',
-      `${((endDate - startDate) / 1000).toFixed(2)} second(s)`,
-      filePath,
-      sizeToReadable(compiled.length)
-    );
-  }
+  report(
+    undefined,
+    '🟢',
+    `${((endDate - startDate) / 1000).toFixed(2)} second(s)`,
+    filePath,
+    sizeToReadable(compiled.length)
+  );
 
   return compiled;
 }
