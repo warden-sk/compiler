80136f6f2ea19e6fbf70461b5ac24b3c3aee32e0
Marek Kobida <marek.kobida@gmail.com>
2023-04-02 15:30:08 +0200

    update

--- cssTransformer.js
+++ cssTransformer.js
@@ -6,12 +6,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const typescript_1 = __importDefault(require("typescript"));
-const cache_1 = __importDefault(require("./cache"));
-const report_1 = __importDefault(require("./helpers/report"));
-const sizeToReadable_1 = __importDefault(require("./helpers/sizeToReadable"));
+const compileCss_1 = __importDefault(require("./compileCss"));
 const cssTransformer = (options) => {
     return context => {
         return sourceFile => {
@@ -22,17 +19,7 @@ const cssTransformer = (options) => {
                         if (/\.css/.test(expression.text)) {
                             const FILE_PATH = sourceFile.fileName.replace(/\/[^\/]+$/, '');
                             const CSS_PATH = path_1.default.resolve(FILE_PATH, expression.text);
-                            cache_1.default.set(CSS_PATH, [fs_1.default.readFileSync(CSS_PATH), new Date()]);
-                            const DESIGN_CSS_PATH = './node_modules/@warden-sk/compiler/design.css';
-                            if (cache_1.default.has(DESIGN_CSS_PATH)) {
-                            }
-                            else {
-                                cache_1.default.set(DESIGN_CSS_PATH, [fs_1.default.readFileSync(DESIGN_CSS_PATH), new Date()]);
-                            }
-                            fs_1.default.writeFileSync(path_1.default.resolve(options.outputPath, './index.css'), Object.keys(cache_1.default.storage)
-                                .filter(l => /\.css/.test(l))
-                                .reduce((l, r) => l + cache_1.default.storage[r][0], ''));
-                            (0, report_1.default)(undefined, '\x1b[34m[CSS]\x1b[0m', (0, sizeToReadable_1.default)(cache_1.default.get(CSS_PATH)[0].length), `\x1b[32m${CSS_PATH}\x1b[0m`);
+                            (0, compileCss_1.default)(options.outputPath, CSS_PATH);
                         }
                     }
                 }
--- cssTransformer.ts
+++ cssTransformer.ts
@@ -2,12 +2,9 @@
  * Copyright 2023 Marek Kobida
  */
 
-import fs from 'fs';
 import path from 'path';
 import ts from 'typescript';
-import cache from './cache';
-import report from './helpers/report';
-import sizeToReadable from './helpers/sizeToReadable';
+import compileCss from './compileCss';
 
 interface Options {
   outputPath: string;
@@ -26,28 +23,7 @@ const cssTransformer = (options: Options): ts.TransformerFactory<ts.SourceFile> 
 
               const CSS_PATH = path.resolve(FILE_PATH, expression.text);
 
-              cache.set(CSS_PATH, [fs.readFileSync(CSS_PATH), new Date()]);
-
-              const DESIGN_CSS_PATH = './node_modules/@warden-sk/compiler/design.css';
-
-              if (cache.has(DESIGN_CSS_PATH)) {
-              } else {
-                cache.set(DESIGN_CSS_PATH, [fs.readFileSync(DESIGN_CSS_PATH), new Date()]);
-              }
-
-              fs.writeFileSync(
-                path.resolve(options.outputPath, './index.css'),
-                Object.keys(cache.storage)
-                  .filter(l => /\.css/.test(l))
-                  .reduce((l, r) => l + cache.storage[r][0], '')
-              );
-
-              report(
-                undefined,
-                '\x1b[34m[CSS]\x1b[0m',
-                sizeToReadable(cache.get(CSS_PATH)[0].length),
-                `\x1b[32m${CSS_PATH}\x1b[0m`
-              );
+              compileCss(options.outputPath, CSS_PATH);
             }
           }
         }
