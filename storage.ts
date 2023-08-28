99557ff100862cdab9580ab5941fe2d0b602df2d
Marek Kobida <marek.kobida@gmail.com>
2023-08-28 12:01:24 +0930

    updated `storage.ts`

--- storage.ts
+++ storage.ts
@@ -28,7 +28,7 @@ export const AlignItems = ['baseline', 'stretch', ...SelfPosition] as const;
 // https://drafts.csswg.org/css-align/#propdef-align-self
 export const AlignSelf = ['baseline', 'stretch', ...SelfPosition] as const;
 export const Border = ['0', '1', '2'] as const;
-export const BorderRadius = ['0', '1', '2', '50'] as const;
+export const BorderRadius = ['0', '1', '2', '3', '50'] as const;
 export const Cursor = ['pointer'] as const;
 // https://drafts.csswg.org/css-display-3/#propdef-display
 export const Display = [
