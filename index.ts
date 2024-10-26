f75f4c567c2660632361d1379ddfa13b9156bd2a
Marek Kobida <marek.kobida@gmail.com>
2024-10-26 20:24:55 +0200

    removed `reportErrors` option & renamed `useHtml` to `useHtmlCompiler` & removed unused `projects` constant

--- index.ts
+++ index.ts
@@ -27,12 +27,9 @@ type Options = {
   cache?: Cache;
   outputPath?: string;
   publicPath?: string;
-  reportErrors?: boolean;
-  useHtml?: boolean;
+  useHtmlCompiler?: boolean;
   useTransformers?: boolean;
 };
-
-const projects = new Map<string, number>();
 
 function compile(filePath: string, options: Options): string {
   const startDate: number = +new Date();
@@ -42,66 +39,9 @@ function compile(filePath: string, options: Options): string {
     outputPath: path.resolve(options.outputPath ?? './public'),
   };
 
-  projects.set(updatedOptions.outputPath, +new Date());
-
-  updatedOptions.useHtml && compileHtml(updatedOptions);
+  updatedOptions.useHtmlCompiler && compileHtml(updatedOptions);
 
   const transformers: ts.CustomTransformers = { before: [cssTransformer(updatedOptions), jsTransformer()] };
-
-  if (updatedOptions.reportErrors) {
-    let compiled = '';
-
-    const compilerHost: ts.CompilerHost = ts.createCompilerHost({});
-    compilerHost.writeFile = (fileName, text) => {
-      compiled = text;
-
-      updatedOptions.cache?.set(fileName, [Buffer.from(compiled), new Date()]);
-    };
-
-    const program: ts.Program = ts.createProgram([filePath], compilerOptions, compilerHost);
-
-    const emitResult: ts.EmitResult = program.emit(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      updatedOptions.useTransformers ?
-        /compiler\//.test(filePath) ?
-          undefined
-        : transformers
-      : undefined,
-    );
-
-    const diagnostics: ts.Diagnostic[] = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
-
-    const endDate: number = +new Date();
-
-    if (diagnostics.length > 0) {
-      for (const diagnostic of diagnostics) {
-        if (diagnostic.file) {
-          const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-
-          report(
-            undefined,
-            '\x1b[34m[JS]\x1b[0m',
-            sizeToReadable(compiled.length),
-            `${((endDate - startDate) / 1000).toFixed(2)} second(s)`,
-            `\x1b[31m${diagnostic.file.fileName}\n\n${message}\n\x1b[0m`,
-          );
-        }
-      }
-    } else {
-      report(
-        undefined,
-        '\x1b[34m[JS]\x1b[0m',
-        sizeToReadable(compiled.length),
-        `${((endDate - startDate) / 1000).toFixed(2)} second(s)`,
-        `\x1b[32m${filePath}\x1b[0m`,
-      );
-    }
-
-    return compiled;
-  }
 
   const { outputText: compiled } = ts.transpileModule(fs.readFileSync(filePath).toString(), {
     compilerOptions,
